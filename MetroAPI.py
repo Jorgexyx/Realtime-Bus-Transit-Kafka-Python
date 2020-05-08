8f79933cca8fb894c8f26a597828d2777bc9bf75
jorgexyx <bautisjo@sonoma.edu>
2020-05-08 00:31:53 -0700

    add specific vehicle url

--- MetroAPI.py
+++ MetroAPI.py
@@ -4,8 +4,16 @@ class MetroAPI:
     def __init__(self):
         self.base_url = "https://api.metro.net/agencies/lametro/"
 
-    def get_route_predictions(self, busNumber=6033):
-        predictions_url = self.base_url + "vehicles/" 
-        response = requests.get(predictions_url)
+    #just return first vehicle
+    def get_random_vehicle_locations(self, busNumber=6033):
+        vehicle_url  = self.base_url + "vehicles/" 
+        response = requests.get(vehicle_url)
+        return response.content
+
+    #example of complete url
+    #http://api.metro.net/agencies/lametro/routes/704/vehicles/
+    def get_vehicle_location(self, routeNumber):
+        vehicle_url = self.base_url + "routes/" + str(704) + "/vehicles/"
+        response = requests.get(vehicle_url)
         return response.content
 
