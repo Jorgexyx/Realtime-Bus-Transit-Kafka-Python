25cbfcda78967dd86b56acbb48e79d8d391b8377
jorgexyx <bautisjo@sonoma.edu>
2020-05-08 01:05:35 -0700

    more error checkiung

--- MetroAPI.py
+++ MetroAPI.py
@@ -4,18 +4,15 @@ class MetroAPI:
     def __init__(self):
         self.base_url = "https://api.metro.net/agencies/lametro/"
 
-    #just return first vehicle
-    #todo impement
-    def get_random_vehicle_locations(self, busNumber=6033):
+    def get_all_vehicle_locations(self):
         vehicle_url  = self.base_url + "vehicles/" 
         response = requests.get(vehicle_url)
-        return response.content
+        return response.json()["items"]
 
     #example of complete url
     #http://api.metro.net/agencies/lametro/routes/704/vehicles/
-    def get_vehicle_location(self, routeNumber=704):
-        vehicle_url = self.base_url + "routes/" + str(routeNumber) + "/vehicles/"
+    def get_vehicle_location(self, routeNumber="704"):
+        vehicle_url = self.base_url + "routes/" + routeNumber + "/vehicles/"
         response = requests.get(vehicle_url)
-
         return [] if len(response.json()["items"]) == 0 else  response.json()["items"][0]
 
--- main.py
+++ main.py
@@ -8,14 +8,16 @@ def main():
 
     if len(sys.argv) != 2:
         raise Exception('Please pass in only one argument. the route id')
-
+    route_number = sys.argv[1]
     client = TransitKafkaClient()
     BusStream = MetroAPI()
     while True:
-        transit_data = BusStream.get_vehicle_location()
+        transit_data = BusStream.get_vehicle_location(route_number)
         if len(transit_data) == 0:
-            print("Transit is not operating. Exiting")
+            print("Transit is not operating. Exiting \n Available routes are: ")
+            print(BusStream.get_all_vehicle_locations())
             return
+
         message = json.dumps(transit_data)
         client.produce_message(message.encode('ascii'))
         time.sleep(.5)
